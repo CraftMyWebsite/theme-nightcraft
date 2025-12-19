fa1840862d65fd2b6b8e5e639edb399ece857381
zomb <leo.goigoux@gmail.com>
2025-12-19 15:35:37 +0100

    [ADD] New Theme Interface

--- Views/Includes/footer.inc.php
+++ Views/Includes/footer.inc.php
@@ -54,8 +54,7 @@ use CMW\Utils\Website;
                 </form>
                 <?php endif; ?>
                 <div data-cmw-visible="footer:footer_active_condition">
-                    <a class="head-a" href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>cgu" data-cmw="footer:footer_desc_condition_use"></a> /
-                    <a class="head-a" href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>cgv" data-cmw="footer:footer_desc_condition_sale"></a>
+                    <a class="head-a" href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>all_terms" data-cmw="footer:footer_desc_condition_use"></a>
                 </div>
             </div>
         </div>
